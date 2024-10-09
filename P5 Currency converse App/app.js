1f905c64e21bdaa1ba3c1700a41132671c8739fb
Sherrrrryyy <Shaheerkhan090909@gmail.com>
2024-10-09 15:42:07 +0500

    5th

---
+++ P5 Currency converse App/app.js
@@ -0,0 +1,46 @@
+const api = "https://api.exchangerate-api.com/v4/latest/USD";
+
+let search = document.querySelector(".searchBox");
+let convert = document.querySelector(".convert");
+let fromCurrecy = document.querySelector(".from");
+let toCurrecy = document.querySelector(".to");
+let finalValue = document.querySelector(".finalValue");
+let finalAmount = document.getElementById("finalAmountValue");
+let resultFrom;
+let resultTo;
+let searchValueOf;
+
+fromCurrecy.addEventListener('change', (event) => {
+    resultFrom = `${event.target.value}`;
+});
+
+toCurrecy.addEventListener('change', (event) => {
+    resultTo = `${event.target.value}`;
+});
+
+search.addEventListener('input', updateValue);
+
+function updateValue(e) {
+    searchValueOf = e.target.value;
+}
+
+convert.addEventListener("click", getResults);
+
+function getResults() {
+    fetch(`${api}`)
+        .then(currency => {
+            return currency.json();
+        }).then(displayResults);
+}
+
+function displayResults(currency) {
+    let fromRate = currency.rates[resultFrom];
+    let toRate = currency.rates[resultTo];
+    finalValue.innerHTML = ((toRate / fromRate) * searchValueOf).toFixed(2);
+    finalAmount.style.display = "block";
+}
+
+function clearVal() {
+    window.location.reload();
+    document.getElementsByClassName("finalValue").innerHTML = "";
+};
